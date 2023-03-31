b90efd03af4f838b7d8517eac0392e7702d4570d
CyberRim <cyberrim@foxmail.com>
2023-03-31 23:05:35 +0800

    fix(index.ts): fix radarr is be comment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Jackett } from './jackett/jackett';
-// import { Radarr } from './arr/radarr';
+import { Radarr } from './arr/radarr';
 import { log } from './logger/log';
 import { Sonarr } from './arr/sonarr';
 
@@ -7,14 +7,14 @@ import { Sonarr } from './arr/sonarr';
     log.debug(`$NODE_ENV=${process.env.NODE_ENV as string}`);
     const jackett = Jackett.getInstance();
     const indexers = await jackett.getConfiguredIndexers();
-    // const radarr = Radarr.getInstance();
+    const radarr = Radarr.getInstance();
     const sonarr = Sonarr.getInstance();
     if (indexers == null) {
         log.info(`exit`);
         return;
     }
     indexers.forEach(async (indexer) => {
-        // await radarr.postIndexer(indexer);
+        await radarr.postIndexer(indexer);
         await sonarr.postIndexer(indexer);
     });
 })();
